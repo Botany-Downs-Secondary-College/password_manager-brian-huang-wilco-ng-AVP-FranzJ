a805314d86acf19fb841c317778af1023191d2c0
Brian Huang <brian.huang@my.bdsc.school.nz>
2021-03-02 14:00:05 +1300

    name creation complete

--- password_manager.py
+++ password_manager.py
@@ -2,9 +2,8 @@
 # make and show passwords
 # Brian =^._.^=
 
-passwords = []
+logins = {}
 
-<<<<<<< HEAD
 class PasswordManager():
     def __init__(self):
         print("Hello and welcome to pwManage")
@@ -13,7 +12,7 @@ class PasswordManager():
 
     def run(self):
         self.age_check()
-        
+        self.create_login()
         
 
         
@@ -35,13 +34,27 @@ class PasswordManager():
         
 
     def age_check(self):
-        age = self.integer_check("How old are you?")
+        age = self.integer_check("How old are you? ")
         if age < 13:
             print("You are not old enough to use this application.")
             self.cya_lol()
         elif age > 140:
             print("There is no way you are old enough to se this application.")
             self.cya_lol()
+
+
+
+    
+    def create_login(self):
+        create_name = True
+
+        while create_name == True:
+            username = input("What would you like your username to be? ")
+            if username in logins:
+                print("Username %s already exists." % (username))
+                continue
+            create_name = False
+            
 
 def main():
     PasswordManager()
@@ -50,30 +63,3 @@ if __name__ == '__main__':
     main()
 
 
-    
-=======
-def cya_lol()
-    print("goodbye mens))")
-    exit()
-
-def integer_check(question):
-    while True:
-        try:
-            num = int(input(question))
-            if num <= 0:
-                print("Please input a positive number.")
-                continue
-            return num
-        except ValueError:
-            print("Please input a valid number.")            
-    
-
-def age_check():
-    age = integer_check("How old are you?")
-    if age < 13:
-        print("You are not old enough to use this application.")
-        cya_lol()
-    
-
-
->>>>>>> c7faac6ce28b7ea1818030b91b4c8528313a6f3f
